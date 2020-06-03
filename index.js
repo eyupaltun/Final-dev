9f45d45be86158a0389ef408e859e9142a1ed0bb
eyupaltun <57413976+eyupaltun@users.noreply.github.com>
2020-06-03 12:13:03 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var getData = function() {
+    var data = {
+        'item1': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-76.jpg',
+        'item2': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-77.jpg',
+        'item3': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-78.jpg'
+    }
+    return data;
+}
